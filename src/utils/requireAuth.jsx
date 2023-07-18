36a48b7ee9625e3f7d803bccb2c1dcd4dfd2a083
Sanket Upreti <upretisanket5@gmail.com>
2023-07-18 23:39:06 +0545

    removed else statement from auth use effect

--- src/utils/requireAuth.jsx
+++ src/utils/requireAuth.jsx
@@ -1,17 +1,13 @@
 import React, { useEffect } from 'react'
-import { Navigate, useLocation } from 'react-router-dom'
 import { useAuth } from './auth'
 
 const RequireAuth = ({children}) => {
     const auth = useAuth()
-    const location = useLocation()
 
     useEffect(()=>{
         const token = localStorage.getItem('token');
         if(token){
             return auth.login(token)
-        }else{
-            return <Navigate to="/teamname" state={{path: location.pathname}} />
         }
     }, [auth])
 
